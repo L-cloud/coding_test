f628e5433dc676ecd1c108eb0f92c2e19864fbfd
JongRokBaek <whiteleo96@gmail.com>
2021-09-27 13:39:36 +0900

    solved leetcode_3.py

---
+++ leetcode/leetcode_3.py
@@ -0,0 +1,14 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        max_len = 0
+        substring = []
+        for index,char in enumerate(s):
+            if char in substring:
+                substring = substring[substring.index(char) + 1 :]
+            substring.append(char)
+            if max_len < len(substring):
+                max_len = len(substring)
+        return max_len
+
+
+
