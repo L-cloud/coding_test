db25bd8a30509ed595a9409ececb8f254054d964
JongRokBaek <whiteleo96@gmail.com>
2021-09-29 22:37:11 +0900

    solved

--- leetcode/leetcode_125.py
+++ leetcode/leetcode_125.py
@@ -1,10 +1,8 @@
-# import re
-# class Solution:
-#     def isPalindrome(self, s: str) -> bool:
-#         s = re.sub('[^0-9a-zA-Z]',"",s).lower()
-#         return s == s[::-1]
+import re
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = re.sub('[^0-9a-zA-Z]',"",s).lower()
+        return s == s[::-1]
 
-a = "ABC"
-print(id(a), a)
-a = a.lower()
-print(id(a),a)
+# if you want pop()
+# use isalnum() then compare pop() == pop(0) while len(str(num or char)) > 1
