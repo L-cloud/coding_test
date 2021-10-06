8a4122185d61231a6fcb3cf01a61d65ed72e2323
JongRokBaek <whiteleo96@gmail.com>
2021-10-06 16:13:36 +0900

    solved

--- leetcode/leetcode_42.py
+++ leetcode/leetcode_42.py
@@ -1,6 +1,6 @@
 from typing import List
 class Solution:
-    def trap(self, height: List[int]) -> int:
+    def trap(self, height: List[int]) -> int: # my version
         water,max_value,stack= 0,0,[]
         for h in height:
             if stack and stack[-1] < h:
@@ -23,5 +23,34 @@ class Solution:
         return water
 
 
-a = Solution()
-print(a.trap([4,2,0,3,2,5]))
+    def trap(self, height:List[int]) -> int: # using two pointer
+        volume = 0
+        left, right = 0, len(height) - 1
+        left_max, right_max = height[left], height[right]
+
+        while left <right:
+            left_max, right_max = max(height[left], left_max), max(height[right], right_max)
+            # move to high
+            if left_max <= right_max:
+                volume += left_max - height[left]
+                left += 1
+            else:
+                volume += right_max - height[right]
+                right -= 1
+        return volume
+
+    def trap(self, height:List[int]) -> int: #using stack
+        stack = []
+        volume = 0
+        for i in range(len(height)):
+            while stack and height[i] > height[stack[-1]]:
+                top = stack.pop()
+                if not len(stack):
+                    break
+                distance = i - stack[-1] - 1
+                waters = min(height[i], height[stack[-1]]) - height[top]
+
+                volume += distance * waters
+            stack.append(i)
+        return volume
+
