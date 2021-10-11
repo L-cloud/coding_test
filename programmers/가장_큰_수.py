4db79bac49082f9b29678cdd69bb8f260334a94d
JongRokBaek <whiteleo96@gmail.com>
2021-10-11 14:24:26 +0900

    unsolved

--- programmers/가장_큰_수.py
+++ programmers/가장_큰_수.py
@@ -1,16 +1,25 @@
+import re
 import collections
+
 def solution(numbers):
-    dic,numbers = collections.defaultdict(list), list(map(str,numbers))
+    dic, numbers = collections.defaultdict(list), list(map(str, numbers))
     answer = ""
     for num in numbers:
         dic[num[0]].append(num)
-    for key in sorted(dic.keys(),reverse = True):
-        num = sorted(dic[key], key = func, reverse = True)
-        for n in num:
-            answer += n    
+    for key in sorted(dic.keys(), reverse=True):
+        if dic[key] != []:
+            num = sorted(dic[key], key=lambda x: (func(x), len(x)), reverse=True)
+            print(num)
+            for n in num:
+                answer += n
+
+    if re.sub('0', '', answer) == '':
+        return '0'
+
     return answer
+
 
 def func(x):
     while len(x) < 4:
         x = x + x[0]
-    return x # 0 <= number <= 1000
+    return x  # 0 <= number <= 1000
