c84c17cc1518421725a98d3bb68d1bfe924a44bf
JongRokBaek <whiteleo96@gmail.com>
2021-10-17 18:44:12 +0900

    solved

--- programmers/best_album.py
+++ programmers/best_album.py
@@ -5,6 +5,5 @@ def solution(genres, plays):
         cnt[genres[i]] += plays[i]
         dic[genres[i]].append([i, plays[i]])
     for key, value in cnt.most_common():
-        for index, play in sorted(dic[key], key = lambda  x: (-x[1] , x[0]))[:2]:
-            answer.append(index)
+        answer += [index for index, play in sorted(dic[key], key = lambda  x: (-x[1] , x[0]))[:2]]
     return answer
