4a4d4887d814efe0abe1deadbee75e492ffeeb05
JongRokBaek <whiteleo96@gmail.com>
2021-10-24 09:32:06 +0900

    solved

--- leetcode/leetcode_24.py
+++ leetcode/leetcode_24.py
@@ -5,7 +5,7 @@ class ListNode:
         self.val = val
         self.next = next
 class Solution:
-    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]: ## Change only value
         if not head:
             return
         slow, fast = head, head.next
@@ -13,3 +13,15 @@ class Solution:
             slow.val, fast.val = fast.val, slow.val
             slow, fast = fast.next, fast.next.next if fast.next else None
         return head
+
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]: # change link
+        if not head or not head.next:
+            return head
+        slow ,fast, root = head, head.next, head.next
+        while fast:
+            slow.next, fast.next = fast.next, slow
+            prev = slow
+            slow, fast = slow.next, slow.next.next if slow.next else None
+            prev.next = fast if fast else slow
+        return root
