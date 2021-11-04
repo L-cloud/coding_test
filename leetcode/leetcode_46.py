8f4a22d2004a0767041fdae7ccb2b6171a2f1d1e
JongRokBaek <whiteleo96@gmail.com>
2021-11-04 22:47:45 +0900

    solved

--- leetcode/leetcode_46.py
+++ leetcode/leetcode_46.py
@@ -1,6 +1,25 @@
 import itertools
-
+from typing import List
 
 class Solution(object):
     def permute(self, nums):
         return list(itertools.permutations(nums))
+
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        results = []
+        prev_elements = []
+
+        def dfs(elements):
+            if len(elements) == 0:
+                results.append(prev_elements[:])
+
+            for e in elements:
+                next_elements = elements[:]
+                next_elements.remove(e)
+
+                prev_elements.append(e)
+                dfs(next_elements)
+                prev_elements.pop()
+
+        dfs(nums)
+        return results
