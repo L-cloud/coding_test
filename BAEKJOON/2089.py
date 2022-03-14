24ef645b0201a51ca89fd4f7468035b2d55c3587
JongRokBaek <whiteleo96@gmail.com>
2022-03-14 11:41:16 +0900

    solving

--- BAEKJOON/2089.py
+++ BAEKJOON/2089.py
@@ -1,16 +1,28 @@
 import sys
-
 num = int(sys.stdin.readline())
-power_of_num = 0
-if num < 0:
-    while (-2) ** power_of_num > num:
-        power_of_num += 1
 
-elif 0 < num:
-    while (-2) ** power_of_num < num:
-        power_of_num += 1
-else:
-    print(0)
-    exit()
+flag = False if num % 2 == 0 else True
+num = num -1 if flag else num # 홀수 짝수 구분
+def dfs(num : int) -> int:
+    exp = 0
+    if num < 0:
+        while (-2) ** exp > num:
+            exp += 1
+    else:
+        while (-2) ** exp < num:
+            exp += 1
 
-num_list = [0] * (power_of_num + 1)
+    return exp, (-2) ** exp - num
+
+exp, num = dfs(num)
+binary = [0] * (exp + 1)
+binary[exp] = 1
+
+while num != 0:
+    exp, num = dfs(num)
+    binary[exp] = 1
+
+if flag:
+    binary[0] = 1
+
+print("".join(binary[::-1]))
