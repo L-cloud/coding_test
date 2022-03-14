a29ab35c03e8b9d180178896d4e6e386b946e149
Jongrok Baek <whiteleo96@gmail.com>
2022-03-14 18:46:49 +0900

    unsolved 22-03-14

--- BAEKJOON/2089.py
+++ BAEKJOON/2089.py
@@ -1,28 +1,17 @@
+# refer to https://en.wikipedia.org/wiki/Negative_base
 import sys
-num = int(sys.stdin.readline())
 
-flag = False if num % 2 == 0 else True
-num = num -1 if flag else num # 홀수 짝수 구분
-def dfs(num : int) -> int:
-    exp = 0
-    if num < 0:
-        while (-2) ** exp > num:
-            exp += 1
+def negaternary(i: int) -> str:
+    """Decimal to negaternary."""
+    if i == 0:
+        digits = ["0"]
     else:
-        while (-2) ** exp < num:
-            exp += 1
+        digits = []
+        while i != 0:
+            i, remainder = divmod(i, -2)
+            if remainder < 0:
+                i, remainder = i + 1, remainder + 2
+            digits.append(str(remainder))
+    return "".join(digits[::-1])
 
-    return exp, (-2) ** exp - num
-
-exp, num = dfs(num)
-binary = [0] * (exp + 1)
-binary[exp] = 1
-
-while num != 0:
-    exp, num = dfs(num)
-    binary[exp] = 1
-
-if flag:
-    binary[0] = 1
-
-print("".join(binary[::-1]))
+print(negaternary(int(sys.stdin.readline())))
