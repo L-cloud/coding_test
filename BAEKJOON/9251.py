4f61da182b75fba598bb9f1e5200bbacd2e087e1
JongRokBaek <whiteleo96@gmail.com>
2022-07-04 23:33:26 +0900

    07/04/22 solved with answer

---
+++ BAEKJOON/9251.py
@@ -0,0 +1,15 @@
+import sys
+# DP 같은데
+A = sys.stdin.readline().rstrip()
+B = sys.stdin.readline().rstrip()
+
+dp = [[0 for _ in range(len(B) + 1)] for _ in range(len(A) + 1)]
+
+for i1,v1 in enumerate(A, 1):
+    for i2,v2 in enumerate(B, 1):
+        if v1 == v2: # 둘이 같은 경우
+            dp[i1][i2] =  dp[i1 - 1][i2 -1] + 1
+        else:
+            dp[i1][i2] = max(dp[i1-1][i2], dp[i1][i2 -1])
+print(dp[-1][-1])
+
