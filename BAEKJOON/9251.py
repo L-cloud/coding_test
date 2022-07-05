d2e5afe9d498d7bf7da2f1cc72abbbc0bf5826af
JongRokBaek <whiteleo96@gmail.com>
2022-07-05 11:19:13 +0900

    07/05/22 solved

--- BAEKJOON/9251.py
+++ BAEKJOON/9251.py
@@ -3,13 +3,25 @@ import sys
 A = sys.stdin.readline().rstrip()
 B = sys.stdin.readline().rstrip()
 
-dp = [[0 for _ in range(len(B) + 1)] for _ in range(len(A) + 1)]
+dp = [0 for _ in range(len(A + 1))]
+for v1 in B:
+    cnt = 0
+    for i2,v2 in enumerate(A):
+        if cnt < dp[i2]:
+            cnt = dp[i2]
+        elif v1 == v2 :
+            dp[i2] = cnt + 1
+print(max(dp))
 
-for i1,v1 in enumerate(A, 1):
-    for i2,v2 in enumerate(B, 1):
-        if v1 == v2: # 둘이 같은 경우
-            dp[i1][i2] =  dp[i1 - 1][i2 -1] + 1
-        else:
-            dp[i1][i2] = max(dp[i1-1][i2], dp[i1][i2 -1])
-print(dp[-1][-1])
+# dp = [[0 for _ in range(len(B) + 1)] for _ in range(len(A) + 1)]
+
+# for i1,v1 in enumerate(A, 1):
+#     for i2,v2 in enumerate(B, 1):
+#         if v1 == v2: # 둘이 같은 경우
+#             dp[i1][i2] =  dp[i1 - 1][i2 -1] + 1
+#         else:
+#             dp[i1][i2] = max(dp[i1-1][i2], dp[i1][i2 -1])
+# print(dp[-1][-1])
+
+
 
