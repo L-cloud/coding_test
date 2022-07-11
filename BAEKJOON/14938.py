a6393b40573a408020065b38d77633161abe8b73
JongRokBaek <whiteleo96@gmail.com>
2022-07-11 15:30:52 +0900

    unsolved

---
+++ BAEKJOON/14938.py
@@ -0,0 +1,28 @@
+import sys, collections
+from typing import List
+input = sys.stdin.readline
+def dfs(m:int,node:int,item:int,distance:List[int]) -> None:
+    for road,cost in roads[node]:
+        if cost <= m and m - cost < distance[road]:
+            if distance[road] == 16: # 이전에 방문 x
+                distance[road] = m-cost
+                item += dfs(m-cost,road, items[road],distance) # item을 더해준다
+            else:
+                distance[road] = m-cost
+                item += dfs(m-cost,road,0,distance) # 아이템을 더하지 않고 방문
+    return item
+n,m,r = map(int, input().split())
+items = list(map(int,input().split()))
+roads = collections.defaultdict(list)
+max_item = 0
+for _ in range(r):
+    a,b,l = map(int,input().split())
+    roads[a-1].append([b-1,l])
+    roads[b-1].append([a-1,l])
+for i in range(n):
+    distance = [16 for  _ in range(n)]
+    distance[i] = -1
+    max_item = max(dfs(m,i,items[i],distance),max_item)
+
+print(max_item)
+
