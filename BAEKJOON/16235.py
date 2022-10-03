672c79373efab5b553056b92cca5d3d1c45588fc
JongRokBaek <whiteleo96@gmail.com>
2022-10-03 16:14:48 +0900

    논리는 같으니 시간초과

--- BAEKJOON/16235.py
+++ BAEKJOON/16235.py
@@ -2,9 +2,13 @@ from typing import List
 from collections import defaultdict,deque
 import sys
 input = sys.stdin.readline
-def spring(trees:dict[List[int]],land:List[List[int]]) -> List[List[int]]:
-    d_t = []
-    b = []
+N, M, K = map(int, input().split())
+trees  = defaultdict(deque)
+original = [list(map(int,input().split()))for _ in range(N)] # 원래 영양분
+land = [[5]*N for i in range(N)] # 현재 영양분
+answer = K
+def spring(trees:dict[List[int]],land:List[List[int]],b:List[List[int]]) -> List[List[int]]:
+    global answer
     for key in trees:
         for i,t in enumerate(trees[key]):
             if t <= land[key[0]][key[1]]:
@@ -15,20 +19,22 @@ def spring(trees:dict[List[int]],land:List[List[int]]) -> List[List[int]]:
             else:
                 for _ in range(len(trees[key]) - i): # 몇 그루가 살아남았나
                     land[key[0]][key[1]] += trees[key].pop() // 2
+                    answer -= 1
                 break
-    return b
+    return 
 
 
 def fall(b:List[int],trees:dict[List[int]], N:int) -> None:
+    global answer
     dx = [-1,1,0,0,1,1,-1,-1]
     dy = [0,0,1,-1,1,-1,1,-1]
-    tempt = []
     for row,col in b:
         for k in range(8):
             x = dx[k] + row
             y = dy[k] + col
             if 0<= x < N and 0<= y < N:
                 trees[(x,y)].appendleft(1)
+                answer += 1
     return
 
 def winter(original:List[List[int]], land:List[List[int]]) ->None :
@@ -37,10 +43,8 @@ def winter(original:List[List[int]], land:List[List[int]]) ->None :
             land[i][j] += original[i][j]
     return
     
-N, M, K = map(int, input().split())
-trees  = defaultdict(deque)
-original = [list(map(int,input().split()))for _ in range(N)] # 원래 영양분
-land = [[5]*N for i in range(N)] # 현재 영양분
+
+f=[]
 for _ in range(M):
     a,b,c = map(int,input().split())
     trees[(a-1,b-1)].append(c)
@@ -48,8 +52,8 @@ for _ in range(K):
     if not trees:
         print(0)
         exit()
-    b = spring(trees, land) # 죽은 나무 영양분 return
-    fall(b,trees,N)
+    spring(trees, land,f) 
+    fall(f,trees,N)
     winter(original,land)
 
-print(sum(len(trees[key]) for key in trees))
+print(answer)
